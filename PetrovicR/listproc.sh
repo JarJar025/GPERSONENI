208319e5fcf4372470a5f04916be1c6a058abe80
root <root@jessie64>
2017-09-12 17:59:32 +0200

    finMardi

--- PetrovicR/listproc.sh
+++ PetrovicR/listproc.sh
@@ -4,20 +4,13 @@
 		affiche	
 	}	
 function affiche()
-{
+{	
+	printf "%s"
 	lstpid=`ls /proc |grep -E ^[0-9]`
 		for repetition in $lstpid
 		do
-			anwser=`awk '/^Name/ {printf "%s",$2} /^State/ {gsub("\(|\)"," ",$3); printf "%s",$3} /^Pid/{printf "%s\n",$2} ' /proc/$repetition/status`
-	
-			if $anwser == `awk'/^awk' $anwser`
-			then
-				$anwser>>/bin/nul			
-			
-			else
-			
-				echo "$anwser"
-			fi
+			awk '/^Name/ {printf "%s",$2} /^State/ {gsub("\(|\)"," ",$3); printf "%s",$3} /^Pid/ {printf "%s\n",$2} ' /proc/$repetition/status 2> /dev/null
+		
 		done
 }
 main $@
