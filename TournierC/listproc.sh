3ac11be47ddc99ccd1710312c0f6ff9277ee7f95
clementtournier <ctournier@delfingen.com>
2017-09-19 08:58:10 +0200

    Version 5

--- TournierC/listproc.sh
+++ TournierC/listproc.sh
@@ -5,18 +5,18 @@
 
 #ALPHA VERSION
 
-printf "\n%-20s %-20s %-20s\n" **Name** **Pid** **State**
+printf "\n%-20s %-20s %-20s\n" **PID** **NAME** **STATE**
 
 function new_ps()
 {
-	processus_rep=`ls /proc | grep -E ^[0-9]`
+	local processus_rep=`ls /proc | grep -E ^[0-9]`
 	
 	for rep in $processus_rep
 	do
 		if [ -e /proc/$rep/status ] 
 		then
 			process=$(awk '/^Pid|^Name/ {printf "%s ",$2} /^State/ {gsub("\(sleeping\)","SLEEPING", $3); printf "%s ",$3}' /proc/$rep/status)
-			echo $process | awk '{printf "%-20d %-20s %-20s\n",$3,$2,$1}' >> /tmp/test_listproc.txt 	
+			echo $process | awk '{printf "%-20d %-20s %-20s\n",$3,$1,$2}' >> /tmp/test_listproc.txt 	
 #			name=`grep -s "Name" /proc/$rep/status | awk '{print $NF}'`
 #			pid=`grep -s "^Pid" /proc/$rep/status | awk '{print NF}'`
 #			state=`grep -s "State" /proc/$rep/status | awk '{print $2 $3}'`
@@ -24,7 +24,7 @@ function new_ps()
 		fi
 	done
 	cat /tmp/test_listproc.txt | sort -nk 1
-	rm /tmp/test_listproc.txt
+	rm -f /tmp/test_listproc.txt
 }
 
 function main()
