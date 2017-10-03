b1995ba3cf0b4e014babfef81da05cf113eb0366
clementtournier <ctournier@delfingen.com>
2017-10-03 07:21:49 +0200

    ImportACL

--- TournierC/listproc.sh
+++ TournierC/listproc.sh
@@ -1,33 +1,30 @@
 #!/bin/bash
 
 #This script allow you to list every enabled process
-#listed by Name, Pid, and State
+#listed by Pid, Name, and State
 
 #ALPHA VERSION
 
-#TEMPORARY CODE
-#process=$(awk '/^Pid|State|Name/ {printf "%-25s%25s%25s "%s "$2 "$3}' /proc/$rep/status)
-
-printf "\n%-25s%25s%25s\n" **Name** **Pid** **State**
+printf "\n%-20s %-20s %-20s %-20s %-20s\n" **PID** **PPid** **NAME** **STATE**
 
 function new_ps()
 {
-	processus_rep=`ls /proc | grep -E ^[0-9]`
+	local processus_rep=`ls /proc | grep -E ^[0-9]`
 	
 	for rep in $processus_rep
 	do
 		if [ -e /proc/$rep/status ] 
 		then
-			process=$(awk '/^Pid|^Name/ {printf "%s ",$2} /^State/ {gsub("\(sleeping\)","SLEEPING", $3); printf "%s ",$3}' /proc/$rep/status)
-#			process=$(awk '/^Pid|State|Name/ {print $2 $3}' /proc/$rep/status)
-			echo $process
-#			name=`grep -s "Name" /proc/$rep/status | awk '{print $NF}'`
-#			pid=`grep -s "^Pid" /proc/$rep/status | awk '{print NF}'`
-#			state=`grep -s "State" /proc/$rep/status | awk '{print $2 $3}'`
-#			printf "%-25s%25s%25s\n "$name "$pid "$state
+			local process=$(mawk '/^Pid|^Name|^PPid/ {printf "%s ",$2} /^State/ {gsub("\(sleeping\)","SLEEPING", $3); printf "%s ",$3}' /proc/$rep/status)
+			echo $process | awk '{printf "%-20d %-20d %-20s %-20s\n",$3,$4,$1,$2}' >> /tmp/test_listproc.txt 	
 		fi
-	done
+	done 
+
+	cat /tmp/test_listproc.txt | sort -nk 1
+	rm /tmp/test_listproc.txt
 }
+
+#	for childs in /proc/*/status
 
 function main()
 {
